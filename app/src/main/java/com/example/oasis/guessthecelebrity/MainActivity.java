43a0eaf2a3aec3f2425647588b202046f3a4d2f6
Agano oasis <oasisagano@gmail.com>
2019-01-15 14:28:14 +0900

    Basic structure that gets images and celebrities

--- app/src/main/java/com/example/oasis/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/oasis/guessthecelebrity/MainActivity.java
@@ -1,13 +1,113 @@
 package com.example.oasis.guessthecelebrity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebUrls = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+
+    // Async class to download the data
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // task caller
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("http://www.posh24.se/kandisar").get();
+            // get data in a certain div
+            String[] splitResult = result.split("<div class=\"listedArticles\">");
+
+            //parse out the img path
+            Pattern p = Pattern.compile("img src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+            while (m.find()) {
+                // append the returned links to the urls array
+                celebUrls.add(m.group(1));
+            }
+            // parse out artist name
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+            while (m.find()) {
+                // append the returned names to the names array
+                celebNames.add(m.group(1));
+            }
+
+            //Log.i("Before the link",splitResult[0]);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            //getting the url passed in
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+
+                }
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+
+            }
+
+
+        }
+    }
+
+    // Image downloader class, it uses the urls given from the array then get the images
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
